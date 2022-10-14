72d256d4453918a05adfe8b67a8411634d876816
daveramseymusic <63751809+daveramseymusic@users.noreply.github.com>
2022-10-14 09:46:56 -0400

    error importing get_movie_recs function into the index file

--- movie_recommender/movie_recommender.py
+++ movie_recommender/movie_recommender.py
@@ -4,7 +4,10 @@
 __all__ = ['get_movie_recs', 'search_movies_n_recommend']
 
 # %% ../nbs/00_movie_recommender.ipynb 18
-def get_movie_recs(full_title):
+def get_movie_recs(full_title:str, # String saved to `favorite_movie` variable
+                   learn,# Trained Model
+                   titles #DataFrame with all movie titles from dls
+                        ):
     "This function will use the `nn.CosineSimilarity` on the 50 latent factors for each movie to find the 30 movies most similar to your favorite movie."
     movie_factors = learn.model.i_weight.weight
     idx = int(titles[titles.title == full_title].midx)
@@ -12,8 +15,10 @@ def get_movie_recs(full_title):
     idx = distances.argsort(descending=True)
     return [o for o in titles.title[idx.tolist()][:30]]
 
-# %% ../nbs/00_movie_recommender.ipynb 19
-def search_movies_n_recommend(favorite_movie:str # The movie title typed into the `gr.Textbox()` that the user will see on the `gradio` app
+# %% ../nbs/00_movie_recommender.ipynb 20
+def search_movies_n_recommend(favorite_movie:str, # The movie title typed into the `gr.Textbox()` that the user will see on the `gradio` app
+                             learn, # Trained model
+                              titles, # Movie titles df
                              ):
     "This function returns 30 recommendations using `get_movie_recs` after searching through all movie titles to find any titles that contain the words in the `favorite_movie` variable."
     movies_found = ''
@@ -38,7 +43,7 @@ def search_movies_n_recommend(favorite_movie:str # The movie title typed into th
     output_str =  movies_found+'\n\n'+explainer
     
     #get recommendations from model
-    recommendations = get_movie_recs(full_title) 
+    recommendations = get_movie_recs(full_title, learn,titles) 
     #create list of all the recommendations to print
     print_lst = ''
     for o in recommendations:    print_lst =print_lst+str(o) + '\n'
